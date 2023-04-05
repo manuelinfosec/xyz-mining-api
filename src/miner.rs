08323ad38797221cb96511c53e34b58cc15e0ddc
Manuel <manuelinfosec@gmail.com>
2023-04-05 03:24:51 +0100

    Create request/database structs

---
+++ src/miner.rs
@@ -0,0 +1,26 @@
+use serde::{Deserialize, Serialize};
+
+// json payload
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Miner {
+    pub id: String,
+    pub address: String,
+    pub club_name: String,
+    pub hash_rate: i32, // MH/s
+    pub shares_mined: i32,
+}
+
+// post request for new miner
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NewMiner {
+    nickname: String,
+}
+
+// database object: DAO (database access object)
+pub struct MinderDAO {
+    pub id: String,
+    pub address: String,
+    pub nickname: String,
+    pub hash_rate: i32,
+    pub shares_mined: i32,
+}
