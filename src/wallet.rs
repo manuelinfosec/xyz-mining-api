bb57c29fae55c21d2718ea89949860960cf18262
Manuel <manuelinfosec@gmail.com>
2023-04-05 03:32:32 +0100

    Create wallet structs

---
+++ src/wallet.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+
+// json payload
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Wallet {
+    pub address: String,
+    pub club_name: String,
+    pub total_hash_rate: i32,
+    pub total_shares_mined: i32,
+    pub total_workers_online: i32,
+    pub workers_online: Vec<Miner>,
+}
+
+// post request body for new miner
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NewWalletRequst {
+    club_name: String,
+}
+
+// DAO object
+pub struct WalletDAO {
+    pub address: String, 
+    pub club_name: String,
+}
