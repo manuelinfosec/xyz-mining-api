717567ada6c0c0db56b0fca643ed4e9afbaeeeee
Manuel <manuelinfosec@gmail.com>
2023-04-15 00:23:32 +0100

    Create Actix HttpServer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate actix_web;
+
 mod wallet;
 mod miner;
 mod util;
@@ -6,7 +9,28 @@ mod controllers {
     pub mod wallet;
 }
 
+use {
+    actix_web::{middleware, App, HttpServer},
+    std::{env, io},
+};
 
-fn main() {
-    println!("Hello, world!");
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(controllers::wallet::list_wallets)
+            .service(controllers::wallet::get_wallet)
+            .service(controllers::wallet::create_wallet)
+            .service(controllers::miner::list_miners)
+            .service(controllers::miner::get_miner)
+            .service(controllers::miner::create_miner)
+    })
+        .bind("0.0.0.0:9090").expect("Could not start server")
+        .run()
+        .await
 }
